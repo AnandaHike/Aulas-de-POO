3bb47041307b9d24c0b2189ba17a9de1020a75c8
AnandaHike <62773469+AnandaHike@users.noreply.github.com>
2020-10-04 15:50:16 -0300

    Create Diciplina.java
    Criação da Classe Disciplina

---
+++ AvaliacaoP1/src/java/br/edu/fatecpg/poo/Diciplina.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fatecpg.poo;
+
+/**
+ *
+ * @author nanda
+ */
+public class Diciplina {
+    
+    private String nome;
+    private String ementa;
+    private String ciclo;
+    private double nota;
+
+    public Diciplina(String nome, String ementa, String ciclo) {
+        this.nome = nome;
+        this.ementa = ementa;
+        this.ciclo = ciclo;
+    }
+    
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmenta() {
+        return ementa;
+    }
+
+    public void setEmenta(String ementa) {
+        this.ementa = ementa;
+    }
+
+    public String getCiclo() {
+        return ciclo;
+    }
+
+    public void setCiclo(String ciclo) {
+        this.ciclo = ciclo;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    
+    
+    
+}
